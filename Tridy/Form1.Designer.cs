01bff1987641824d65c76597d4c1088cddc49e73
RobinKristofcak <kristofcakr.03@spst.eu>
2022-03-21 00:23:06 +0100

    Přidejte soubory projektu.

---
+++ Tridy/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Tridy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(29, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hmotnost nákladu:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Class";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Tridy/NakladniAuto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tridy
+{
+   
+
+    class NakladniAuto
+    {
+        private string SPZ;
+        private int nosnost;
+        private int hmotnost = 0;
+
+
+        public NakladniAuto()
+        {
+            this.SPZ = "ABNC454";
+            this.nosnost = 10;
+        }
+
+        public string NalozNaklad(int nosnost, int hmotnost)
+        {
+            if (hmotnost < nosnost)
+            {
+                string t = "Bylo naloženo " + hmotnost + "t nákladu.";
+                return t;
+            }
+            else
+            {
+                string t = "Bylo naloženo " + nosnost + "t nákladu (protoze se vice neveslo)."; ;
+                return t;
+            }
+        }
+
+        public int VylozNaklad(int nosnost, int hmotnost)
+        {
+
+            hmotnost = hmotnost - hmotnost;
+            return hmotnost;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+   
+}
